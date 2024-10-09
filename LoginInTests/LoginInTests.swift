baffb09b17e69880c8a180155deaf023823bedbc
MaybeRT-rt <maybe.rt@icloud.com>
2024-10-10 01:41:10 +0300

    Initial Commit

---
+++ LoginInTests/LoginInTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginInTests.swift
+//  LoginInTests
+//
+//  Created by Liz-Mary on 10.10.2024.
+//
+
+import Testing
+@testable import LoginIn
+
+struct LoginInTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
